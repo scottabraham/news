d266460ffb8c68437278c81c0e7ff7539af1701e
scottabraham <scottabraham13@yahoo.co.uk>
2018-05-20 18:17:00 -0230

    Majority of PWA work now done

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,10 @@
 const staticAssets = [
     './',
-    '/static/css',
-    '/static/js/bundle.js'
+    './fallback.json',
+    './images/loading.gif',
+    './images/fetch_dog.jpg',
+    './styles/news.css',
+    './static/js/bundle.js'
 ];
 
 self.addEventListener('install', async event=>{
@@ -14,4 +17,33 @@ self.addEventListener('install', async event=>{
 
 self.addEventListener('fetch', event=>{
     console.log('Service Worker has intercepted a request')
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.origin){
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req));
+    }
+    
 })
+
+async function cacheFirst(req){
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req){
+    const cache = await caches.open('news-articles');
+
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+        
+    } catch (error) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse || await caches.match('./fallback.json');
+    }
+
+}
